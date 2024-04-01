7a411c02a1b27124c9adec8ce42b7ad8f6d7d845
Cameron <greayham@gmail.com>
2024-04-01 13:11:40 +1300

    Initial Commit

---
+++ MapTag/MapTagApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapTagApp.swift
+//  MapTag
+//
+//  Created by Cam Graham on 01/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct MapTagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
