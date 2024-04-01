79e4e02bd56022c220fe1f9ee1e6c1d4a714a6e3
Cameron <greayham@gmail.com>
2024-04-01 17:41:08 +1300

    Create base level views

---
+++ MapTag/MapHome.swift
@@ -0,0 +1,18 @@
+//
+//  MapHome.swift
+//  MapTag
+//
+//  Created by Cam Graham on 01/04/2024.
+//
+
+import SwiftUI
+
+struct MapHome: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapHome()
+}
---
+++ MapTag/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  MapTag
+//
+//  Created by Cam Graham on 01/04/2024.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
