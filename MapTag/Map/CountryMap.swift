450e5556af768ea017efcb9f879c9fef52df2828
Cameron <greayham@gmail.com>
2024-04-20 00:06:33 +1200

    Create timeline scroller + animate to corresponding location

--- MapTag/Map/CountryMap.swift
+++ MapTag/Map/CountryMap.swift
@@ -21,7 +21,7 @@ struct CountryMap: View {
     @State var mapCam: MapCameraPosition = .region((MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: -40.900557, longitude: 174.885971), span: MKCoordinateSpan(latitudeDelta: 5, longitudeDelta: 5))))
     
     @State var monthGroupedImages: [Int: [MapTagImage]] = [:]
-
+    
     @State var zoom: CGFloat = 0.0
     var scale: CGFloat {
         if zoom < 100_000 {
@@ -33,15 +33,32 @@ struct CountryMap: View {
         }
     }
     
+    @State var height: Double = 0
+    
+    var textHeight: Double {
+        height / Double(Calendar.current.monthSymbols.count)
+    }
+    
+    @State var dragPos: Double = 0.0
+    
+    var currentMonthInt: Int {
+        Int(dragPos.rounded(.down))
+    }
+    
+    var currentLocation: CLLocationCoordinate2D? {
+        monthGroupedImages[currentMonthInt]?.first?.getImageCoords()
+    }
+    
+    @State var animateCamera = false
+    
     var body: some View {
         ZStack {
             Map(position: $mapCam, bounds: MapCameraBounds(centerCoordinateBounds: mapRegion, maximumDistance: calculatedCameraHeight), interactionModes: [.pan, .zoom], scope: innerLoc) {
-
                 ForEach(Array(monthGroupedImages.keys), id: \.self) { monthInt in
                     // TODO: get averaged location
                     if let firstImage = monthGroupedImages[monthInt]?.first, let coord = firstImage.getImageCoords() {
                         Annotation(monthInt.getMonthString(), coordinate: coord) {
-
+                            
                             PhotoAnnotation(image: firstImage.image)
                                 .frame(width: scale * 100, height: scale * 100)
                         }
@@ -49,8 +66,6 @@ struct CountryMap: View {
                     }
                 }
             }
-            
-
             .mapStyle(.hybrid(elevation: .realistic,
                               pointsOfInterest: PointOfInterestCategories.including(pointsOfInterest),
                               showsTraffic: false))
@@ -59,10 +74,12 @@ struct CountryMap: View {
                     zoom = mapCam.camera.distance
                 }
             })
-            
-            
-//            .standard(elevation: .realistic, emphasis: .automatic, pointsOfInterest: PointOfInterestCategories.including(pointsOfInterest), showsTraffic: false)
-            
+            .mapCameraKeyframeAnimator(trigger: animateCamera) { mapCamera in
+                KeyframeTrack(\.centerCoordinate) {
+                    CubicKeyframe(currentLocation!, duration: 1)
+                }
+            }
+
             HStack {
                 VStack {
                     Button(action: {
@@ -77,16 +94,46 @@ struct CountryMap: View {
                 }
                 Spacer()
             }
+            
+            HStack {
+                Spacer()
+                VStack {
+                    ForEach(Calendar.current.monthSymbols.indices, id: \.self) { index in
+                        TimelineView(timeString: Calendar.current.monthSymbols[index], index: index, dragPos: dragPos, textHeight: textHeight)
+                    }
+                }
+                .coordinateSpace(name: "Dates")
+                .overlay(content: {
+                    GeometryReader(content: { geometry in
+                        Rectangle().opacity(0.2)
+                            .onAppear {
+                                height = geometry.size.height
+                            }
+                    })
+                })
+                .gesture(DragGesture(minimumDistance: 0, coordinateSpace: .named("Dates")).onChanged({ dragValue in
+                    var index = dragValue.location.y / textHeight
+                    dragPos = index
+                }))
+                .sensoryFeedback(.increase, trigger: currentMonthInt)
+            }
         }
         .onAppear(perform: {
             monthGroupedImages = photoVM.monthGroupedImages(images: photoVM.locationGroupedImages[location.country] ?? [])
         })
+        .onChange(of: currentLocation) { oldValue, newValue in
+            // TODO: use old value to calc animation
+            if newValue != nil {
+                animateCamera.toggle()
+            }
+        }
     }
 }
 
-//#Preview {
-//    CountryMap(location: TaggedLocation(country: "New Zealand", location: CLLocation(latitude: -40.900557, longitude: 174.885971)))
-//}
+#Preview {
+    CountryMap(startExploring: .constant(true),location: TaggedLocation(country: "New Zealand", location: CLLocation(latitude: -40.900557, longitude: 174.885971)), mapRegion: MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: -41.837132505080696, longitude: 172.79331092869865), span: MKCoordinateSpan(latitudeDelta: 24.752210992836861, longitudeDelta: 19.233723900868455)), calculatedCameraHeight: 4890336.595950017)
+        .environmentObject(PhotoSelectionViewModel())
+}
 
 extension Int {
     func getMonthString() -> String {
@@ -94,3 +141,12 @@ extension Int {
         return calender.monthSymbols[self]
     }
 }
+
+extension CLLocationCoordinate2D: Equatable {
+    public static func == (lhs: CLLocationCoordinate2D, rhs: CLLocationCoordinate2D) -> Bool {
+        lhs.latitude == rhs.latitude && lhs.longitude == rhs.longitude
+    }
+    
+    
+}
+
